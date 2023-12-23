80c3c1115ecbcd2f64ea7000f61cdfd8c1de4474
Teminioluwa <teminiwilliam2020@gmail.com>
2023-12-22 22:36:48 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-/**
- * main Programming is like building a multilingual puzzle
- *Return : 0 (success)
- */
- int main(void) 
-{
-    // Using puts to print a string
+
+int main() {
+    // Using puts to print the specified string
     puts("Programming is like building a multilingual puzzle");
 
-    // End the program with the value 0
+    // Returning success (0)
     return 0;
 }
 
