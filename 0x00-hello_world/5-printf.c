e8a78007b1841fc9ea0da73d8069a0623db2c87a
Teminioluwa <teminiwilliam2020@gmail.com>
2023-12-23 00:03:54 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ *with proper grammar, but the outcome is a piece of art,
+ */
+int main(void) 
+{
+    printf("%s, but the outcome is a piece of art,\n", "This is a simple C program");
+    return (0);
+}
+
