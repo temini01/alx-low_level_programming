6ba6588d529807a183cc06f05b588738d2c523e1
Teminioluwa <teminiwilliam2020@gmail.com>
2023-12-23 03:33:53 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*
- *with proper grammar, but the outcome is a piece of art,
+/**
+ * main - a program that uses printf not puts
+ * Return:0(Success);
  */
-int main(void) 
+int main(void)
 {
-    printf("%s, but the outcome is a piece of art,\n", "This is a simple C program");
-    return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
-
