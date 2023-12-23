865317a2a75c4d86deea5fb2d224bad3d0648243
Teminioluwa <teminiwilliam2020@gmail.com>
2023-12-22 22:25:56 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Using puts to print a string
+    puts("Programming is like building a multilingual puzzle");
+
+    // End the program with the value 0
+    return 0;
+}
+
