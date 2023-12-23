524d68e5d651366160145be7a547677ff81963f7
Teminioluwa <teminiwilliam2020@gmail.com>
2023-12-22 22:57:12 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
- * main "Programming is like building a multilingual puzzle"
+/**
+ * main - A "Programming is like building a multilingual puzzle"
  *Return: 0 (Success)
-*/
+ */
 int main(void) 
 {
-    puts("Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
